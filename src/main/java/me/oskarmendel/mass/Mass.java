e91773d9ceb9be1869ee3f9e7893a926b20764e9
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-31 19:07:31 +0200

    Initial project commit.

---
+++ src/main/java/me/oskarmendel/mass/Mass.java
@@ -0,0 +1,11 @@
+package me.oskarmendel.mass;
+
+/**
+ * Created by Oskar on 2017-05-31.
+ */
+public class Mass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
