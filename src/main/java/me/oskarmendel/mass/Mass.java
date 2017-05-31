cc1ca19021187149f1d66359cdf190e3b9bfd96f
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-31 19:40:10 +0200

    Added LWJGL boilerplate code.

--- src/main/java/me/oskarmendel/mass/Mass.java
+++ src/main/java/me/oskarmendel/mass/Mass.java
@@ -1,11 +1,150 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Oskar Mendel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.oskarmendel.mass;
 
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 /**
- * Created by Oskar on 2017-05-31.
+ *
+ * @author Oskar Mendel
+ * @version 0.00.00
+ * @name Mass.java
  */
 public class Mass {
 
+    // The window handle
+    private long window;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        new Mass().run();
+    }
+
+    /**
+     *
+     */
+    public void run() {
+        init();
+        loop();
+
+        // Free window callbacks and destroy window
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    /**
+     *
+     */
+    public void init() {
+        // Setup and error callback.
+        // Will print error messages in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        //Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(300, 300, "Mass", NULL, NULL);
+        if ( window == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        // Setup key callback
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                glfwSetWindowShouldClose(window, true);
+            }
+        });
+
+        // Get the thread stack and push new frame
+        try (MemoryStack stack = stackPush()){
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            // Get window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // Get resolution of primary monitor
+            GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(window, (vidMode.width() - pWidth.get(0)) / 2,
+                    (vidMode.height() - pHeight.get(0))/2);
+        }
+
+        // Make OpenGL context current
+        glfwMakeContextCurrent(window);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make window visible
+        glfwShowWindow(window);
+    }
+
+    /**
+     *
+     */
+    public void loop() {
+        // Makes the OpenGL bindings available for use.
+        GL.createCapabilities();
+
+        // Set clear color
+        glClearColor(0.5f, 0.0f, 0.5f, 0.0f);
+
+        // Run the rendering loop until the user attempts to close
+        while (!glfwWindowShouldClose(window)) {
+            // Clear the frame buffer.
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            // Swap the color buffer
+            glfwSwapBuffers(window);
+
+            // Poll for window events.
+            glfwPollEvents();
+        }
     }
 }
